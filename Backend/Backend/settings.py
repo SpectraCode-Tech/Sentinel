3d6b2222e04c69a47e74f02f2dcea1f7cb900fc8
Oladimeji <dimejirsq08@gmail.com>
2026-03-07 12:26:22 +0100

    nn

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",
-     "http://127.0.0.1:5174",
+    "http://127.0.0.1:5174",
     "http://localhost:5175",
     "http://127.0.0.1:5175",
     "http://localhost:5173",
