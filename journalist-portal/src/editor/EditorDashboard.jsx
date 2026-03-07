9e47541c6d7486e0efa4845c72e9148a1c83a6b3
Oladimeji <dimejirsq08@gmail.com>
2026-03-07 15:24:22 +0100

    editor

--- journalist-portal/src/editor/EditorDashboard.jsx
+++ journalist-portal/src/editor/EditorDashboard.jsx
@@ -29,7 +29,7 @@ export default function EditorDashboard() {
   };
 
     useEffect(() => {
-        API.get("articles/dashboard/editor/")
+        API.get("dashboard/editor/")
             .then(res => {
                 setUsername(res.data.username);
                 setStats(res.data);
