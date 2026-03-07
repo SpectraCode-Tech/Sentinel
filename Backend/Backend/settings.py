37a45235881443c3958cf02a5f8a6931ca45ef1a
Oladimeji <dimejirsq08@gmail.com>
2026-03-07 11:12:07 +0100

    back

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -23,6 +23,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5175",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://sentinel-pi-one.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
