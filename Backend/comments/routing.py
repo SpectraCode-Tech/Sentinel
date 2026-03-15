7a64d61a266aaa6d97e29713253296daf47d9563
Oladimeji <dimejirsq08@gmail.com>
2026-03-15 15:34:03 +0100

    asgi

--- Backend/comments/routing.py
+++ Backend/comments/routing.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from .consumers import CommentConsumer
 
 websocket_urlpatterns = [
-    # Fixed: article_id now comes BEFORE comments
+    # Remove any extra prefixes if they aren't in your frontend URL
+    # This matches: wss://sentinel-ou6m.onrender.com/ws/articles/89/comments/
     re_path(r'^ws/articles/(?P<article_id>\d+)/comments/$', CommentConsumer.as_asgi()),
 ]
