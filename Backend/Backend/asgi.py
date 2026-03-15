3a4007fccbc8189d0ac98444482e6c9f2e3e99ed
Oladimeji <dimejirsq08@gmail.com>
2026-03-15 16:05:53 +0100

    okay

--- Backend/Backend/asgi.py
+++ Backend/Backend/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import comments.routing # Replace with your app name
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- Backend/comments/routing.py
+++ Backend/comments/routing.py
@@ -3,7 +3,5 @@ from django.urls import re_path
 from .consumers import CommentConsumer
 
 websocket_urlpatterns = [
-    # Remove any extra prefixes if they aren't in your frontend URL
-    # This matches: wss://sentinel-ou6m.onrender.com/ws/articles/89/comments/
     re_path(r'^ws/articles/(?P<article_id>\d+)/comments/$', CommentConsumer.as_asgi()),
 ]
