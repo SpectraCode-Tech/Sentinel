89f9d637d49a460c634d2709174b1ffcd76e1c34
Oladimeji <dimejirsq08@gmail.com>
2026-04-23 12:11:38 +0100

    nnn

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -112,30 +112,13 @@ ASGI_APPLICATION = "Backend.asgi.application"
 
 AUTH_USER_MODEL = "accounts.User"
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         ssl_require=True
-#     )
-# }
-
-import os
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django_cockroachdb',
-        'NAME': 'defaultdb',
-        'USER': 'dimeji',
-        'PASSWORD': 'n9KIyFwbeOeAYSQAf7PCeQ',
-        'HOST': 'sentinel-25306.j77.aws-eu-central-1.cockroachlabs.cloud',
-        'PORT': '26257',
-        'OPTIONS': {
-            'sslmode': 'verify-full',
-            'sslrootcert': "system", 
-            'options': '--cluster=sentinel-25306', 
-        },
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        ssl_require=True
+    )
 }
+
 
 
 
